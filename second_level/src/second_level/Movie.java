769ec5208a6a9028faa9c6987bf9fa376e5618c0
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-07-11 15:05:46 +0200

    write properties, getters & setters for Movie class

--- second_level/src/second_level/Movie.java
+++ second_level/src/second_level/Movie.java
@@ -2,4 +2,78 @@ package second_level;
 
 public class Movie {
 
+    private String title;
+    private String director;
+    private Integer lengthInMinute;
+    private String category;
+    private String rating;
+    private Integer budget;
+    private Float imdbRating;
+
+    public Movie(String title, String director, Integer lengthInMinute, String category, String rating, Integer budget, Float imdbRating) {
+        this.title = title;
+        this.director = director;
+        this.lengthInMinute = lengthInMinute;
+        this.category = category;
+        this.rating = rating;
+        this.budget = budget;
+        this.imdbRating = imdbRating;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public Float getImdbRating() {
+        return imdbRating;
+    }
+
+    public Integer getLengthInMinute() {
+        return lengthInMinute;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setBudget(Integer budget) {
+        this.budget = budget;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setImdbRating(Float imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public void setLengthInMinute(Integer lengthInMinute) {
+        this.lengthInMinute = lengthInMinute;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
 }
